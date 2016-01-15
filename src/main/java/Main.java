cc29aef456769d9d798aa7f46be3be10ef6d4253
XieZihan <ZihanX@XieZihans-MacBook.local>
2016-01-14 20:39:44 -0500

    Demo_energy_zix8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,11 +27,21 @@ public class Main {
 
     //get("/hello", (req, res) -> "Hello World");
 
-	get("/hello", (req, res) -> {
+	/*get("/hello", (req, res) -> {
       RelativisticModel.select();
       Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
       return "E=mc^2: 12 GeV = " + m.toString();
+    });*/
+	
+	get("/hello", (req, res) -> {
+      RelativisticModel.select();
+
+      String energy = System.getenv().get("ENERGY");
+
+      Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
+      return "E=mc^2: " + energy + " = " + m.toString();
     });
+	
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
